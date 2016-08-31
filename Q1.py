af7fdcf075594e5e7b08c26bb17e258db1e8968d
Sushrut <sushrut91@gmail.com>
2016-08-31 13:19:01 -0400

    First working version

---
+++ Q1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import math
+
+def main():
+    try:
+        a = int(input("Enter the value of a: "))
+        p = int(input("Enter the value of p: "))
+        
+        if a < 0 or p < 0:
+            print ('Values of a and p should be greater than 0')
+        else:
+            mod_val = a % p
+            print('a mod p =', mod_val)
+            
+            if a > p:
+                print('Value of a < p')
+            else:
+                if isPrime(p):
+                    rev_mod = (a ** (p-2)) % p
+                    print('Reverse modulus of of a is: ',rev_mod)
+                    check_ans = (a * rev_mod) % p
+                    print('Checking the answer a * a(inverse) = ({} * {}) % {} yealds answer : {}'
+                          .format( a, rev_mod, p,check_ans ))
+                else:
+                    print('p should be a prime number to calculate a inverse')
+        
+    except ValueError:
+        print('Please enter valid integer values only for a & p')
+    except ZeroDivisionError:
+        print('Please enter p > 0')
+  
+
+#Function to check if number is prime or not    
+def isPrime(n):
+    flag = True
+    for i in range(2,int(math.sqrt(n))):
+        if n % i == 0:
+            flag = False
+    return flag  
+if __name__ == "__main__": main()
