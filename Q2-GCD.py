e79d97765c1f77a9abd6a9b0ed43627b15b9c0ca
Sushrut <sushrut91@gmail.com>
2016-09-09 11:29:39 -0400

    Code refactoring

--- Q2-GCD.py
+++ Q2-GCD.py
@@ -5,11 +5,11 @@ def main():
         n1 = int(input('Enter an integer: '))
         n2 = int(input('Enter an integer: '))
         if n1 == 0 or n2 == 0:
-            print('Enter n1 and n2 both greater than 0')
+            print('Enter n1 and n2 both should be greater than 0')
             return -1 # Return error
         else:
             gcd = GCD(n1,n2)
-            print('The G.C.D of {} and {} is {}'.format(n1, n2, gcd))
+            print('\nThe G.C.D of {} and {} is {}'.format(n1, n2, gcd))
             return 0
             
     except ValueError:
@@ -17,6 +17,11 @@ def main():
 
 #Function which returns GCD of both values            
 def GCD(n1,n2):
+    # Convert negative values to positive values
+    if n1 < 0:
+        n1 = -n1
+    if n2 < 0:
+        n2 = -n2
     #Ensure that n2 is always greater
 
     if n2 > n1:
--- Q3ApphineCipher.py
+++ Q3ApphineCipher.py
@@ -2,31 +2,37 @@
 import re
 def main():
     letters = {'a':0, 'b':1, 'c':2 , 'd':3 , 'e':4, 'f':5 , 'g':6, 'h':7, 'i':8, 'j':9, 'k':10, 'l':11, 'm':12, 'n':13, 
-               'o':14, 'p':15 , 'q':16 , 'r':17, 's':18 , 't':19 , 'u':20, 'v':21, 'w':22 , 'x':23, 'y':24 , 'z':25, 'A':26
-               ,'B':27, 'C':28, 'D':29 , 'E':30, 'F':31, 'G':32 , 'H':33 , 'I':34, 'J':35 , 'K':36 ,'L':37 ,'M':38 ,'N':39,
-               'O':40, 'P':41, 'Q':42 ,'R':43 ,'S':44 ,'T':45 ,'U':46, 'V':47, 'W':48, 'X':49, 'Y':50 ,'Z':51 ,' ':52}
+               'o':14, 'p':15 , 'q':16 , 'r':17, 's':18 , 't':19 , 'u':20, 'v':21, 'w':22 , 'x':23, 'y':24 , 'z':25,' ':26}
     #a is the key and b denotes the shift taking place
     a ,b = 5,8
     
-    input_string = input('Enter a string to encrypt containing alphabets.Numbers and special characters are not allowed. Space is allowed: ')
+    input_string = input('Enter a string to encrypt containing alphabets.\nNumbers and special characters are not allowed. \nSpace is allowed: ')
     match_val = re.match("^[a-zA-Z ]*$", input_string, 0)
     
-    if match_val is not None:
-        cipher_string,decrypted_plain_string = '',''
-        
-        for letter in input_string:
-            cipher_string = cipher_string + EncryptCharacter(letter, letters, a, b)
-        
-        print ('Encrypted string : ', cipher_string)
-        
-        for c_letter in cipher_string:
-            decrypted_plain_string = decrypted_plain_string + DecryptCharacter(c_letter, letters, a, b)
+    try:
+        a = int(input('Enter key1 (a): '))
+        b = int(input('Enter key2 (b): '))
+        if match_val is not None:
+            cipher_string,decrypted_plain_string = '',''
             
-        print('Value of a :' ,a)
-        print('Value of b :' ,b)
-        print ('Decrypted original string :', decrypted_plain_string)
-    else:
-        print('You entered a invalid character')      
+            if AreRelativePrime(a, len(letters)):
+                for letter in input_string:
+                    cipher_string = cipher_string + EncryptCharacter(letter, letters, a, b) 
+                        
+                print ('Encrypted string : ', cipher_string)      
+                
+                for c_letter in cipher_string:
+                    decrypted_plain_string = decrypted_plain_string + DecryptCharacter(c_letter, letters, a, b)           
+                print('Value of a :' ,a)
+                print('Value of b :' ,b)
+                print ('Decrypted original string :', decrypted_plain_string)
+            else:
+                print('a and n, i.e length of charset should be co-prime numbers')
+        else:
+            print('You entered a invalid character')
+    except ValueError:
+        print('Only integer values are allowed')   
+           
 #Function to decrypt the characters
 def DecryptCharacter(encrypt_char, letter_set, a, b):
     #P = a'(x)-b mod p
@@ -34,18 +40,15 @@ def DecryptCharacter(encrypt_char, letter_set, a, b):
     n = len(letter_set)
     plain_char = encrypt_char
     encrypt_char_num = letter_set[encrypt_char]
-    
-    if AreRelativePrime(a, n):
-        a_inverse = (a ** (n-2)) % n
-        plain_char = (a_inverse * (encrypt_char_num - b)) %n
+ 
+    a_inverse = getModularInverse(a, n)
+    plain_char = (a_inverse * (encrypt_char_num - b)) %n
         
         #Find the key from value 
-        for key,value in letter_set.items():
-            if value == plain_char:
-                return str(key)
-    else:
-        return "Please change value of a such that a & n are coprime numbers, n is a prime number, n>a"
-
+    for key,value in letter_set.items():
+        if value == plain_char:
+            return str(key)
+        
 #Function to encrypt an input character     
 def EncryptCharacter(plain_char, letter_set,a,b):
     # E(x) = (a(x) + b) mod p where a and p should be co-prime
@@ -54,29 +57,19 @@ def EncryptCharacter(plain_char, letter_set,a,b):
     cipher_char = plain_char
     
     if AreRelativePrime(a,n):
-        cipher_char = (a*x + b) % n
-        
-        #Find the key from value 
-    for key,value in letter_set.items():
-        if value == cipher_char:
-            return str(key)
-            
-        
-    else:
-        return 'Please change value of a such that a & n are coprime numbers, n is a prime number, n>a'
+        cipher_char = (a*x + b) % n 
+        for key,value in letter_set.items():
+            if value == cipher_char:
+                return str(key)
         
 def AreRelativePrime(n1,n2):
 # Numbers are relative prime if their GCD is 1, Euclid's algorithm
 # Ensure n1 is always greater
 # Ensure that n1 is always prime
-    flag = False
-    
-    if n1 > n2:
-        flag = isPrime(n1)
-    elif n2> n1:
-        flag = isPrime(n2)
-    else:
-        return 'Both a and n cannot be equal for algorithm to work'
+    flag = True
+    if n1 == n2:
+        print('a and be cannot be equal.')
+        return False
         
     if flag:
         if n2 > n1 :
@@ -93,13 +86,16 @@ def AreRelativePrime(n1,n2):
         else:
             return False
     else:
-        return False    
-def isPrime(n):
-    flag = True
-    for i in range(2,n):
-        if n % i == 0:
-            flag = False
-    return flag
+        return False
+
+#Calculate multiplicative inverse       
+def getModularInverse(a,m):
+    x=1
+    while True:
+        if (a * x) % m == 1  :
+            break
+        x=x+1
+    return x
     
 #Run the function main() at startup of the script    
 if __name__ == "__main__": main()
